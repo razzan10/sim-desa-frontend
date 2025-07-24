4510e4599efacf73e803a5b1c3993bc01d59bee7
razzan10 <mrazzan9@gmail.com>
2025-07-24 17:41:30 +0700

    Initial commit of project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import Navbar from "./components/navbar";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,132 @@
+function Navbar() {
+  return (
+    <nav>
+      <h1>DesaKu</h1>
+      <ul>
+        <li>
+          <a href="/">Beranda</a>
+        </li>
+        <li>
+          <a href="/profilDesa">Profil Desa</a>
+          <ul>
+            <li>
+              <a href="#">Visi Misi</a>
+            </li>
+            <li>
+              <a href="#">Sejarah</a>
+            </li>
+            <li>
+              <a href="#">Peta Desa</a>
+            </li>
+            <li>
+              <a href="#">Sturktur Organisasi Desa</a>
+            </li>
+            <li>
+              <a href="#">Potensi Alam</a>
+            </li>
+            <li>
+              <a href="#">Produk Desa</a>
+            </li>
+            <li>
+              <a href="#">Fasilitas Umum</a>
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="/PPID">PPID</a>
+          <ul>
+            <li>
+              <a href="#">Profil PPID</a>
+            </li>
+            <li>
+              <a href="#">Maklumat Pelayanan Informasi Publik</a>
+            </li>
+            <li>
+              <a href="#">Jenis Informasi Desa</a>
+            </li>
+            <li>
+              <a href="#">Struktur PPID</a>
+            </li>
+            <li>
+              <a href="#">Regulasi PPID</a>
+            </li>
+            <li>
+              <a href="#">Panduan Pelayan Desa</a>
+            </li>
+            <li>
+              <a href="#">Alur Permohonan Informasi</a>
+            </li>
+            <li>
+              <a href="#">Pengajuan Keberatan Informasi</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="/lembaga">Lembaga</a>
+          <ul>
+            <li>
+              <a href="#">Badan Permusyawaratan Desa</a>
+            </li>
+            <li>
+              <a href="#">Lembaga Pemberdayaan Masyarakat</a>
+            </li>
+            <li>
+              <a href="#">Pembinaan Kesejahteraan Keluarga</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="/informasi">Informasi</a>
+          <ul>
+            <li>
+              <a href="#">Berita Desa</a>
+            </li>
+            <li>
+              <a href="#">Data Desa</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="/publikasi">Publikasi</a>
+          <ul>
+            <li>
+              <a href="#">APBDes</a>
+            </li>
+            <li>
+              <a href="#">RPJMDes</a>
+            </li>
+            <li>
+              <a href="#">RKPDes</a>
+            </li>
+            <li>
+              <a href="#">RAPBDes</a>
+            </li>
+            <li>
+              <a href="#">Alokasi Dana Desa</a>
+            </li>
+            <li>
+              <a href="#">Dana Desa</a>
+            </li>
+            <li>
+              <a href="#">Produk Hukum Desa</a>
+            </li>
+            <li>
+              <a href="#">Surat Keputusan Kepala Desa</a>
+            </li>
+            <li>
+              <a href="#">Alokasi Dana Desa Mandiri</a>
+            </li>
+            <li>
+              <a href="#">Dana Desa Mandiri</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="/administrasi">Administrasi Persuratan</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+export default Navbar;
